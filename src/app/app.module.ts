e9c6a167eb6be492511abd8fcba4d10b8262a142
Christopher Utsch <christopher.utsch@tu-dresden.de>
2020-03-21 11:48:55 +0100

    implemented routing and added home module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,36 @@ import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import {SellerViewModule} from "../seller-view/seller-view.module";
+import {SellerViewComponent} from "../seller-view/seller-view/seller-view.component";
+import { HomeComponent } from './home/home.component';
+
+const appRoutes: Routes = [
+  { path: 'seller/:id', component: SellerViewComponent },
+  { path: 'home', component: HomeComponent},
+  { path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  //{ path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     SellerViewModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
