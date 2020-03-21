5961f56f4656e3ef2ea5148afe9dca01aa1fe8c5
Christopher Utsch <christopher.utsch@tu-dresden.de>
2020-03-21 09:58:49 +0100

    Setup complete with material design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -12,7 +13,8 @@ import {SellerViewModule} from "../seller-view/seller-view.module";
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SellerViewModule
+    SellerViewModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
